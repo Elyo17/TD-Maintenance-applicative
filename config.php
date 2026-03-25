dfb0c5292037b15d638ffba82d5a20a45f3e2faf
Elyo <maamarielyo@gmail.com>
2026-03-25 17:58:34 +0100

    ajout de fichiers

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Fichier de configuration
+ * Le fichier config.php ne doit JAMAIS être versionné
+ */
+
+// Configuration de la base de données
+define('DB_HOST', 'mysql-maamari.alwaysdata.net');
+define('DB_USER', 'maamari_annonces');
+define('DB_PASS', 'Alousse17074&');
+define('DB_NAME', 'maamari_annonces_db');
+?>
